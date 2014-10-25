334c4166bcae3953479d104ae03152692b399404
Mikhail Denisenko <denisenkom@gmail.com>
2014-10-25 00:32:18 -0400

    Simplified setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,12 @@ import re
 import sys
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
-from setuptools import setup, find_packages
-
-
-def read(*parts):
-    with open(os.path.join(os.path.dirname(__file__), *parts)) as handle:
-        return handle.read()
+from setuptools import setup
 
 
 def find_version(*file_paths):
-    version_file = read(*file_paths)
+    with open(os.path.join(os.path.dirname(__file__), *file_paths)) as handle:
+        version_file = handle.read()
     version_match = re.search("^__version__ = ['\"]([^'\"]*)['\"]",
                               version_file, re.M)
     if version_match:
@@ -20,107 +16,15 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-# Provided as an attribute, so you can append to these instead
-# of replicating them:
-standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak')
-standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
-                                './dist', 'EGG-INFO', '*.egg-info')
-
-
-# (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
-# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
-# Note: you may want to copy this into your setup.py file verbatim, as
-# you can't import this from another package, when you don't know if
-# that package is installed yet.
-def find_package_data(
-    where='.', package='',
-    exclude=standard_exclude,
-    exclude_directories=standard_exclude_directories,
-    only_in_packages=True,
-    show_ignored=False):
-    """
-    Return a dictionary suitable for use in ``package_data``
-    in a distutils ``setup.py`` file.
-
-    The dictionary looks like::
-
-        {'package': [files]}
-
-    Where ``files`` is a list of all the files in that package that
-    don't match anything in ``exclude``.
-
-    If ``only_in_packages`` is true, then top-level directories that
-    are not packages won't be included (but directories under packages
-    will).
-
-    Directories matching any pattern in ``exclude_directories`` will
-    be ignored; by default directories with leading ``.``, ``CVS``,
-    and ``_darcs`` will be ignored.
-
-    If ``show_ignored`` is true, then all the files that aren't
-    included in package data are shown on stderr (for debugging
-    purposes).
-
-    Note patterns use wildcards, or can be exact paths (including
-    leading ``./``), and all searching is case-insensitive.
-    """
-
-    out = {}
-    stack = [(convert_path(where), '', package, only_in_packages)]
-    while stack:
-        where, prefix, package, only_in_packages = stack.pop(0)
-        for name in os.listdir(where):
-            fn = os.path.join(where, name)
-            if os.path.isdir(fn):
-                bad_name = False
-                for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        if show_ignored:
-                            print >> sys.stderr, (
-                                "Directory %s ignored by pattern %s"
-                                % (fn, pattern))
-                        break
-                if bad_name:
-                    continue
-                if (os.path.isfile(os.path.join(fn, '__init__.py'))
-                    and not prefix):
-                    if not package:
-                        new_package = name
-                    else:
-                        new_package = package + '.' + name
-                    stack.append((fn, '', new_package, False))
-                else:
-                    stack.append((fn, prefix + name + '/', package, only_in_packages))
-            elif package or not only_in_packages:
-                # is a file
-                bad_name = False
-                for pattern in exclude:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        if show_ignored:
-                            print >> sys.stderr, (
-                                "File %s ignored by pattern %s"
-                                % (fn, pattern))
-                        break
-                if bad_name:
-                    continue
-                out.setdefault(package, []).append(prefix + name)
-    return out
-
 setup(
     name="django-sqlserver",
     version=find_version("sqlserver", "__init__.py"),
     url='https://github.com/denisenkom/django-sqlserver',
     license='MIT',
-    description="Django backend database support for MS SQL Server.",
-    long_description=read('README.rst'),
+    description="Django backend database support for MS SQL Server and pytds.",
     author='Mikhail Denisenko',
     author_email='denisenkom@gmail.com',
-    packages=find_packages(),
-    package_data=find_package_data(),
+    packages=['sqlserver'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
@@ -131,6 +35,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Database',
     ],
-    install_require=['django-mssql'],
-    zip_safe=False,
+    install_requires=['django-mssql'],
+    zip_safe=True,
 )
